d892e700be9561666f0ff2b0852260a1bb8cd2d5
Geemkuu <geemkuu0@gmail.com>
2026-04-08 17:57:40 +0300

    env

--- api/index.py
+++ api/index.py
@@ -5,9 +5,14 @@ import os
 app = FastAPI()
 
 # Connect to your Supabase
-URL = "https://eymdckclisznzixcdket.supabase.co"
-KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5bWRja2NsaXN6bnppeGNka2V0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTY1MDY1MSwiZXhwIjoyMDkxMjI2NjUxfQ.d9cf0ypTDNVU9YASP91rqvV4mN_xZ_IvJ0qpPtWW98I"
+import os
+
+# This tells the brain to look in the Vercel "Cloud Settings" first
+URL = os.environ.get("https://eymdckclisznzixcdket.supabase.co")
+KEY = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5bWRja2NsaXN6bnppeGNka2V0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTY1MDY1MSwiZXhwIjoyMDkxMjI2NjUxfQ.d9cf0ypTDNVU9YASP91rqvV4mN_xZ_IvJ0qpPtWW98I")
+
 supabase = create_client(URL, KEY)
+
 
 @app.get("/api/balance")
 def get_balance():
